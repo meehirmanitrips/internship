8b5880c96af5bc11c7b444c66069dc7d2f58f614
Meehir Mani Tripathy <meehirmanitrips@gmail.com>
2023-04-19 09:46:33 +0530

    added routing and form

---
+++ CrudApplication/frontend/src/components/AddUser.js
@@ -0,0 +1,80 @@
+import {
+  FormGroup,
+  FormControl,
+  Input,
+  InputLabel,
+  Typography,
+  styled,
+  Button,
+} from "@mui/material";
+import { useState } from "react";
+
+const Container = styled(FormGroup)`
+  width: 50%;
+  margin: 5% auto 5% auto;
+  & > div {
+    margin-top: 20px;
+  }
+`;
+
+const defaultValue = {
+  name: "",
+  age: "",
+  email: "",
+  salary: "",
+  country: "",
+  state: "",
+  city: "",
+};
+
+const AddUser = () => {
+  const [employee, setEmployee] = useState(defaultValue);
+
+  const onValueChange = (e) => {
+    setEmployee({ ...employee, [e.target.name]: e.target.value });
+    console.log(employee);
+  };
+
+  const addEmployeeDetails = () => {};
+
+  return (
+    <Container>
+      <Typography variant="h4">Add Employee</Typography>
+      <FormControl>
+        <InputLabel>Name</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="name" />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Age</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="age" />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Email</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="email" />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Salary</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="salary" />
+      </FormControl>
+      <FormControl>
+        <InputLabel>Country</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="country" />
+      </FormControl>
+      <FormControl>
+        <InputLabel>State</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="state" />
+      </FormControl>
+      <FormControl>
+        <InputLabel>City</InputLabel>
+        <Input onChange={(e) => onValueChange(e)} name="city" />
+      </FormControl>
+      <FormControl>
+        <Button variant="contained" onClick={() => addEmployeeDetails()}>
+          Add Employee
+        </Button>
+      </FormControl>
+    </Container>
+  );
+};
+
+export default AddUser;
---
+++ CrudApplication/frontend/src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { AppBar, Toolbar, styled } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const Header = styled(AppBar)``;
+
+const Tabs = styled(NavLink)`
+  font-size: 20px;
+  margin-right: 20px;
+  color: inherit;
+  text-decoration: none;
+`;
+
+const NavBar = () => {
+  return (
+    <>
+      <Header position="static">
+        <Toolbar>
+          <Tabs to="/">All Employees</Tabs>
+          <Tabs to="/add">Add Employee</Tabs>
+        </Toolbar>
+      </Header>
+    </>
+  );
+};
+
+export default NavBar;
